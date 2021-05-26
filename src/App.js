bf9d803cd347d9acab9150a2d8ec08a42c2c2a77
Rajath Kallichanda <rajathmuthappak@gmail..com>
2021-05-26 13:43:56 -0500

    sdf

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <div className="App">
           <Switch>
             <Route
-              path="/" exact
+              path="my_site/" exact
               render={props => (
                 <Home
                   {...props}
